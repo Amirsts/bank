cc012e451fd82a151d1b19146d2e1554742b532d
Amirsts <amir.a.d.m.u@gmail.com>
2025-06-02 15:49:45 +0330

    package branch changes 1

--- src/branch/Branch.java
+++ src/branch/Branch.java
@@ -1,11 +1,28 @@
 package branch;
 
+import account.Account;
+import person.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Branch  {
 
     private String branchNumber;
     private BranchManager branchManager;
+    private AssistantManager assistantManager;
+    private List<Teller> tellers;
+    private List<Account> accounts;
+    private List<Customer> customers;
 
-    public Branch(){}
+    public Branch(String branchNumber){
+        this.branchNumber = branchNumber;
+        this.tellers = new ArrayList<>();
+        this.accounts = new ArrayList<>();
+        this.customers = new ArrayList<>();
+    }
+
+    //getters & setters
 
     public String getBranchNumber(){
         return branchNumber;
@@ -13,5 +30,76 @@ public class Branch  {
 
     public BranchManager getBranchManager() {
         return branchManager;
+    }
+
+    public void setBranchManager(BranchManager branchManager){
+        this.branchManager  = branchManager ;
+        branchManager.setAssignedBranch(this);
+    }
+
+    public AssistantManager getAssistantManager (){
+        return assistantManager;
+    }
+
+    public void setAssistantManager(AssistantManager assistantManager){
+        this.assistantManager = assistantManager;
+        assistantManager.setAssignedBranch(this);
+    }
+
+
+    public List<Teller> getTellers(){
+        return tellers;
+    }
+
+    public List<Account>  getAccounts(){
+        return accounts;
+    }
+
+    public List<Customer> getCustomers(){
+        return customers;
+    }
+
+
+    //methods
+    public void addTeller(Teller teller){
+        tellers.add(teller);
+        teller.setAssignedBranch(this);
+    }
+
+    boolean removeTeller(String telleId){
+      return tellers.removeIf(t-> t.getEmployeeId().equals(telleId) );
+    }
+
+    public void addAccount(Account account){
+        accounts.add(account);
+    }
+
+    public Account findAccount(String accountnumber){
+        for (Account temp : accounts){
+            if (temp.getAccountNumber().equals(accountnumber)){
+                return  temp;
+            }
+        }
+        return null;
+    }
+
+    public void addCustomer(Customer customer){
+        customers.add(customer);
+    }
+
+    public Customer findCustomer(String nationalCode){
+        for (Customer cu : customers){
+            if (cu.getNationalCode().equals(nationalCode)){
+                return cu;
+            }
+        }
+        return null;
+    }
+    
+    public void displayCustomers(){
+        System.out.println("Customers list:"+ branchNumber + ":");
+        for (Customer cus : customers){
+            System.out.println(cus.getFirstName() + " " + cus.getLastName() + "nationalCode:" + cus.getNationalCode());
+        }
     }
 }
